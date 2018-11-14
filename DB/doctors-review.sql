460ce4782481c5449184cc0b41a9a7bb524357e0
sayedul sayem <sayedulsayem@gmail.com>
2018-11-14 18:26:24 +0600

    updated database

--- DB/doctors-review.sql
+++ DB/doctors-review.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 06, 2018 at 09:03 PM
--- Server version: 10.1.29-MariaDB
--- PHP Version: 7.2.0
+-- Host: localhost
+-- Generation Time: Nov 14, 2018 at 01:25 PM
+-- Server version: 10.1.36-MariaDB
+-- PHP Version: 7.2.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,6 +25,59 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `doctors`
+--
+
+CREATE TABLE `doctors` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `fathers_name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `mothers_name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date_of_birth` date NOT NULL,
+  `dr_degree_id` int(11) NOT NULL,
+  `dr_tags_id` int(11) NOT NULL,
+  `dr_main_ol` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `field` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `explanation` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `drdegrees`
+--
+
+CREATE TABLE `drdegrees` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `license` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `institute` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date_of_passing` date NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `drtags`
+--
+
+CREATE TABLE `drtags` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `specialities` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `chamber` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `available_schedule_d` date DEFAULT NULL,
+  `available_schedule_t` time DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `migrations`
 --
 
@@ -39,9 +92,13 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2014_10_12_000000_create_users_table', 1),
+(1, '2014_10_12_000000_create_superadmins_table', 1),
 (2, '2014_10_12_100000_create_password_resets_table', 1),
-(3, '2018_10_31_201611_create_visitors_table', 1);
+(3, '2018_10_31_201611_create_patients_table', 1),
+(4, '2018_11_13_075746_create_users_table', 1),
+(5, '2018_11_13_081229_create_doctors_table', 1),
+(6, '2018_11_14_095648_create_drdegrees_table', 1),
+(7, '2018_11_14_095706_create_drtags_table', 1);
 
 -- --------------------------------------------------------
 
@@ -58,10 +115,30 @@ CREATE TABLE `password_resets` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Table structure for table `patients`
 --
 
-CREATE TABLE `users` (
+CREATE TABLE `patients` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `date_of_birth` date NOT NULL,
+  `mobile` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `house_no` int(11) NOT NULL,
+  `road` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sub_dist` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `dist` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `zip_code` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `superadmins`
+--
+
+CREATE TABLE `superadmins` (
   `id` int(10) UNSIGNED NOT NULL,
   `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -75,32 +152,42 @@ CREATE TABLE `users` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `visitors`
+-- Table structure for table `users`
 --
 
-CREATE TABLE `visitors` (
+CREATE TABLE `users` (
   `id` int(10) UNSIGNED NOT NULL,
-  `nid` int(11) NOT NULL,
   `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nid` double NOT NULL,
   `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `address` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `date` date NOT NULL,
-  `mobile` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `type` int(11) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `visitors`
---
-
-INSERT INTO `visitors` (`id`, `nid`, `name`, `email`, `address`, `date`, `mobile`, `password`, `created_at`, `updated_at`) VALUES
-(1, 8568, 'sayedul', 'sayedul@gmail.com', 'sldghslgdkh', '2018-11-07', '021651', '$2y$10$lIpNkld7px9YlFn9tOhEx.JctU7TZVYbMkmlXLBB5Nr6a26e/YlHO', '2018-11-06 13:36:53', '2018-11-06 13:36:53');
-
---
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `doctors`
+--
+ALTER TABLE `doctors`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `drdegrees`
+--
+ALTER TABLE `drdegrees`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `drdegrees_license_unique` (`license`);
+
+--
+-- Indexes for table `drtags`
+--
+ALTER TABLE `drtags`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `migrations`
@@ -115,39 +202,71 @@ ALTER TABLE `password_resets`
   ADD KEY `password_resets_email_index` (`email`);
 
 --
+-- Indexes for table `patients`
+--
+ALTER TABLE `patients`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `superadmins`
+--
+ALTER TABLE `superadmins`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `superadmins_email_unique` (`email`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_nid_unique` (`nid`),
   ADD UNIQUE KEY `users_email_unique` (`email`);
-
---
--- Indexes for table `visitors`
---
-ALTER TABLE `visitors`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `doctors`
+--
+ALTER TABLE `doctors`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `drdegrees`
+--
+ALTER TABLE `drdegrees`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `drtags`
+--
+ALTER TABLE `drtags`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `patients`
+--
+ALTER TABLE `patients`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `superadmins`
+--
+ALTER TABLE `superadmins`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `visitors`
---
-ALTER TABLE `visitors`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
