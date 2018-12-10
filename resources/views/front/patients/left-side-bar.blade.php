0efed3c918b46ab356a8bd57405aba181c8baae6
sayedul sayem <sayedulsayem@gmail.com>
2018-12-10 23:20:39 +0600

    left side bar extend 2

--- resources/views/front/patients/left-side-bar.blade.php
+++ resources/views/front/patients/left-side-bar.blade.php
@@ -1,3 +1,4 @@
+<!-- Left side column. contains the logo and sidebar -->
 <aside class="main-sidebar">
     <!-- sidebar: style can be found in sidebar.less -->
     <section class="sidebar">
@@ -26,20 +27,31 @@
         <!-- sidebar menu: : style can be found in sidebar.less -->
         <ul class="sidebar-menu" data-widget="tree">
             <li class="header">MAIN NAVIGATION</li>
-            <li class="treeview">
-                <a href="{{ url('/doctors') }}">
-                    <i class="fa fa-dashboard"></i><span>Dashboard</span>
+            <li class="active treeview menu-open">
+                <a href="#">
+                    <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+                    <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
                 </a>
+                <ul class="treeview-menu">
+                    <li><a href="index.html"><i class="fa fa-circle-o"></i> Dashboard v1</a></li>
+                    <li class="active"><a href="{{ url('/patients') }}"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
+                </ul>
             </li>
             <li class="treeview">
                 <a href="#">
                     <i class="fa fa-files-o"></i>
-                    <span>Practicing Schedule</span>
-                    <span class="pull-right-container"></span>
+                    <span>Layout Options</span>
+                    <span class="pull-right-container">
+              <span class="label label-primary pull-right">4</span>
+            </span>
                 </a>
                 <ul class="treeview-menu">
-                    <li><a href="{{ url('/doctors/all-schedule/create') }}"><i class="fa fa-circle-o"></i>Add Schedule</a></li>
-                    <li><a href="{{ url('/doctors/all-schedule') }}"><i class="fa fa-circle-o"></i>All Schedule In Calendar</a></li>
+                    <li><a href="pages/layout/top-nav.html"><i class="fa fa-circle-o"></i> Top Navigation</a></li>
+                    <li><a href="pages/layout/boxed.html"><i class="fa fa-circle-o"></i> Boxed</a></li>
+                    <li><a href="pages/layout/fixed.html"><i class="fa fa-circle-o"></i> Fixed</a></li>
+                    <li><a href="pages/layout/collapsed-sidebar.html"><i class="fa fa-circle-o"></i> Collapsed Sidebar</a></li>
                 </ul>
             </li>
             <li>
